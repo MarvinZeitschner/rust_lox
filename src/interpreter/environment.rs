1457ac8f97d186351a1f27f122f77a4cf1a3fbff
MarvinZeitschner <m.zeitschner@gmail.com>
2025-03-04 20:49:04 +0100

    feat: add environment enclosing

--- src/interpreter/environment.rs
+++ src/interpreter/environment.rs
@@ -4,15 +4,17 @@ use crate::lex::Token;
 
 use super::{error::RuntimeError, Value};
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Environment<'a> {
     values: HashMap<&'a str, Option<Value>>,
+    enclosing: Option<Box<Environment<'a>>>,
 }
 
 impl<'a> Environment<'a> {
     pub fn new() -> Self {
         Self {
             values: HashMap::new(),
+            enclosing: None,
         }
     }
 
@@ -24,13 +26,16 @@ impl<'a> Environment<'a> {
         }
     }
 
-    pub fn get(&mut self, name: Token<'a>) -> Result<Value, RuntimeError<'a>> {
+    pub fn get(&self, name: Token<'a>) -> Result<Value, RuntimeError<'a>> {
         match self.values.get(name.lexeme) {
             Some(value) => match value {
                 Some(value) => Ok(value.clone()),
                 None => Ok(Value::Nil),
             },
-            None => Err(RuntimeError::UndefinedVariable { name }),
+            None => match &self.enclosing {
+                Some(enclosing) => return enclosing.get(name),
+                _ => Err(RuntimeError::UndefinedVariable { name }),
+            },
         }
     }
 
@@ -42,7 +47,37 @@ impl<'a> Environment<'a> {
                     .and_modify(|v| *v = Some(value));
                 Ok(())
             }
-            false => Err(RuntimeError::UndefinedVariable { name }),
+            false => match &mut self.enclosing {
+                Some(enclosing) => enclosing.assign(name, value),
+                _ => Err(RuntimeError::UndefinedVariable { name }),
+            },
         }
+    }
+}
+
+pub struct EnvironmentBuilder<'a> {
+    encolsing: Option<Box<Environment<'a>>>,
+}
+
+impl<'a> EnvironmentBuilder<'a> {
+    pub fn new() -> Self {
+        Self { encolsing: None }
+    }
+
+    pub fn enclosing(mut self, enclosing: Environment<'a>) {
+        self.encolsing = Some(Box::new(enclosing));
+    }
+
+    pub fn build(&self) -> Environment {
+        Environment {
+            values: HashMap::new(),
+            enclosing: self.encolsing.clone(),
+        }
+    }
+}
+
+impl<'a> Default for EnvironmentBuilder<'a> {
+    fn default() -> Self {
+        Self::new()
     }
 }
